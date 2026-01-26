0a3b0bdc506e78372bcbc6ee9cbe7e3ecc0828d2
ophanim777 <rikyjo49@gmail.com>
2026-01-26 09:25:10 +0700

    taking html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import { fetchWeather, processWeatherData } from "./api";
 import { renderWeather } from "./dom";
+
+const form = document.getElementById("weather-form");
+const input = document.getElementById("location-input");
+const toggleBtn = document.getElementById("toggle-unit");
