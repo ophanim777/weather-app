47502723b80af995a9934430f938d542c9168a02
ophanim777 <rikyjo49@gmail.com>
2026-02-01 15:46:36 +0700

    change render weather function

--- src/dom.js
+++ src/dom.js
@@ -1,13 +1,13 @@
 const display = document.getElementById("weather-display");
 
 function renderWeather(weather) {
+  const display = document.getElementById("weather-display");
+  if (!display) return;
   display.innerHTML = `
     <h2>${weather.address}</h2>
     <p>${weather.description}</p>
     <p>${weather.temperature}°</p>
   `;
-
-  changeBackground(weather.icon);
 }
 
 function changeBackground(icon) {
