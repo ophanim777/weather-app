f36ed77124bc8e562b612b68510a7e2e10fd00a0
ophanim777 <rikyjo49@gmail.com>
2026-02-01 15:55:46 +0700

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-// webpack.config.js
 import path from "node:path";
+import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: "./index.html"
     }),
   ],
   module: {
